72eb33fc7bc2eaa8a8f3c8a1bdbf1667e3db0c7d
Anderson <BlackCode7@outlook.com>
2022-01-24 21:26:17 -0300

    Métodos de retorno métodos com recursividade

--- javaMetodo/src/test/java/javaMetodo/javaMetodo1/ClasseMetodo_1.java
+++ javaMetodo/src/test/java/javaMetodo/javaMetodo1/ClasseMetodo_1.java
@@ -4,14 +4,44 @@ public class ClasseMetodo_1 {
 
 	public String varString = "O filme matrix é o melhor de todos!";
 
-	public String tornaMaiusculoCaracteres(String string) {						
+	public String tornaMaiusculoCaracteres(String string) {	
+		// retorna string em caracteres maiúsculos
 		return varString.toUpperCase();
-	}	
+	}
 
+	public int funcaoRetornaValor(String varString2) {
+		// Retorna o char da posição em que string específica ocupa dentro da string
+		int quantidade = 0;
+		varString2 = varString2.toUpperCase();
+		for (int a = 0; a < varString2.length(); a++) {
+			if(varString2.charAt(a) == 'A') {
+				quantidade++;
+			}
+		}
+		
+		return quantidade;
+	}
 
-	// THIAGO
-	// CINTURA >>> 1,20m
-	// QUADRIL >>> 1,22m
-	
+	public int funcaoMostraPares(int valor) {
+		// Mostra valor par
+		int retorno = 0;		
+		for(int a=0; a<=valor; a=a/2) {
+			retorno += a;
+		}
+		
+		return retorno;
+	}
+
+	public static long calculaFatorial(long fatorial) {
+		// Calcula o fatorial
+		if(fatorial <= 1) {
+			return (1);
+		}else {
+			return (fatorial * calculaFatorial(fatorial - 1));
+		}
+		
+	}
+
+		
 
 }
--- javaMetodo/src/test/java/javaMetodo/metodoJavaTeste1/MetodoJavaTeste1.java
+++ javaMetodo/src/test/java/javaMetodo/metodoJavaTeste1/MetodoJavaTeste1.java
@@ -34,13 +34,73 @@ public class MetodoJavaTeste1 {
 	@Test	
 	@Order(3)
 	@DisplayName("Criar um metodo para converter todos os caracteres da variável em caracteres maiúsculos")
-	public void funcaoParaTornarTodosCaracteresMaiusculos() {
-		
-		String stringMaius = "simone josy";
-		
+	public void funcaoParaTornarTodosCaracteresMaiusculos() {		
+		String stringMaius = "simone josy";		
 		ClasseMetodo_1 classeMetodoTeste = new ClasseMetodo_1();
 		String contaString = classeMetodoTeste.tornaMaiusculoCaracteres(stringMaius); // toString();
-		System.out.println(contaString.length());
-		assertEquals("SIMONE JOSY", (contaString.toUpperCase()));
+		//System.out.println(contaString.length());
+		assertEquals("O FILME MATRIX É O MELHOR DE TODOS!", (contaString.toUpperCase()));
+	} 
+
+	@Test
+	@Order(4)
+	@DisplayName("Testa função que retorna valores")
+	public void funcaoComRetornodeValores() {		
+		ClasseMetodo_1 classeMetodoRetornoTeste = new ClasseMetodo_1();
+		int retornaValor = classeMetodoRetornoTeste.funcaoRetornaValor(classeMetodoRetornoTeste.varString);
+		assertEquals('f', (classeMetodoRetornoTeste.varString).charAt(2));
+		assertEquals(-1, (classeMetodoRetornoTeste.varString.indexOf(retornaValor)));
+		
 	}
+	
+	@Test
+	@Order(5)
+	@DisplayName("Testa calculo de número fatorial")
+	public void metodoRecursivoClaculaFatorial() {
+		ClasseMetodo_1 calculaFatorialMetodo = new ClasseMetodo_1();
+		long calculaFatorialTest = calculaFatorialMetodo.calculaFatorial(5);
+		assertEquals(1, calculaFatorialTest);	
+	}
+	
+	/*@Test
+	@Order(5)
+	@DisplayName("Testa função que retorna valores pares")
+	public void mostrarValores() {
+		ClasseMetodo_1 mostraValor = new ClasseMetodo_1();
+		int funcMostraValorPares = mostraValor.funcaoMostraPares(4);
+		assertEquals(2, String.valueOf(funcMostraValorPares));
+	}*/
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
